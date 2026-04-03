c9ad40dc6938a55bec4539062d22f7639f06bda3
Murtaza Jafri <murtazahusain2006@gmail.com>
2026-04-03 19:00:04 +0500

    adding keen keeper

---
+++ index.js
@@ -0,0 +1,23 @@
+const { Client, GatewayIntentBits, EmbedBuilder, ActivityType } = require('discord.js');
+
+const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent, GatewayIntentBits.GuildMembers]});
+
+client.on('ready', () => {
+    console.log(`Your bot has booted up! ${client.user.tag}`)
+});
+
+client.on('messageDelete', message => {
+    const channel = message.guild.channels.cache.find(ch => ch.name === 'keen-keepers-logs');
+
+    if(!channel) return;
+
+    const embed = new EmbedBuilder()
+    .setTitle('✍️ A Message was deleted.')
+    .setDescription(`**Author** : ${message.author.username} \n**Message** : ${message.content}`)
+    .setColor('#9300A3')
+    .setFooter({ text : `*The Keen Keeper has recorded it.*`})
+
+    channel.send({ embeds: [embed] });
+});
+
+client.login(process.env.TOKEN);
